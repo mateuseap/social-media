dea8807d484d2f4ef844308fa6ed5b50eaa16034
Mateus Elias <meap@cin.ufpe.br>
2023-01-16 18:13:17 -0300

    feat: 'Profile' model created

--- social_media/settings.py
+++ social_media/settings.py
@@ -125,3 +125,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
