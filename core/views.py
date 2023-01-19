87693dc32a019410eca7b7c026d89daed77c6a76
Mateus Elias <meap@cin.ufpe.br>
2023-01-19 01:20:28 -0300

    feat: 'Log Out' feature implemented

--- core/views.py
+++ core/views.py
@@ -55,3 +55,7 @@ def signin(request):
             return redirect('signin')
     else:
         return render(request, 'signin.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
