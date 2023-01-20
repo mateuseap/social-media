69daee0914fcfaa7f40ffeb8d62531fed11e472a
Mateus Elias <meap@cin.ufpe.br>
2023-01-19 23:37:06 -0300

    feat: 'Account Settings' feature implemented partially

--- core/views.py
+++ core/views.py
@@ -26,13 +26,14 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
                 
-                # Log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
                 # Create a Profile object for the new user
 
                 user_model = User.objects.get(username=username) # Getting user object
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
 
         else:
             messages.error(request, 'Password do not match')
@@ -60,3 +61,7 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+@login_required(login_url='signin')
+def settings(request):
+    return render(request, 'settings.html')
